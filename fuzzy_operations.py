421acac721a2a5f6534228ac2ff9528fc6dfa011
vrbaj <Jan.Vrba@vscht.cz>
2017-06-22 10:46:53 +0200

    doplnek

--- fuzzy_operations.py
+++ fuzzy_operations.py
@@ -10,36 +10,49 @@ Contents of this page:
     :depth: 1
 
 
-Fuzzy complement
+Fuzzy complement, union (S-norms) and intersection (T-norms)
 =============================
+
+
 """
 
 import numpy as np
 
 
-def standard_complement(membership_degree):
+def complement_standard(membership_degree):
     complement = 1.0 - membership_degree
     return complement
 
-def sugeno_complement(membership_degree, lambda_value):
+
+def complement_sugeno(membership_degree, lambda_value):
     if lambda_value <= -1:
         print("sugeno_complement:parameter lambda out of range, it should be (-1,inf)")
     complement = (1 - membership_degree) / (1 + lambda_value * membership_degree)
     return complement
 
 
-def yager_complement(membership_degree, w_value):
+def complement_yager(membership_degree, w_value):
+    """
+    Yager
+    """
     if w_value <= 0:
         print("yager_complement: parameter w out of range, it should be (0, inf)")
     complement = (1 - membership_degree ** w_value) ** (1 / w_value)
     return complement
 
 
-"""
-Fuzzy union
-=============================
-"""
+def s_norm_max(membership_degreee_a, membership_degree_b):
+    s_norm_value = max(membership_degreee_a, membership_degree_b)
+    return s_norm_value
 
-testing_membership_degree = 0.9
-complement_value = yager_complement(testing_membership_degree,0.3)
-print(complement_value)
+
+def s_norm_dombi(membership_degree_a, membership_degree_b, params):
+    lambda_value = params[0]
+    a = params[1]
+    b = params[2]
+    s_norm_value = 1 / (1 +((1 / a -1) ** lambda_value))
+
+
+# testing_membership_degree = 0.9
+# complement_value = complement_yager(testing_membership_degree, 0.3)
+# print(complement_value)
